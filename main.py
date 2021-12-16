247617bbe12d0732b50413c969b94a5d11666a3b
Tom Hosiawa <tom.hosiawa@rigelblu.com>
2021-12-16 13:36:02 -0500

    Change indenting to 4 spaces

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ TOKENS_FILEPATH = "./tokens.json"
 # Load dotevn properties
 load_dotenv()
 if os.environ.get("DEBUG").lower() == "true":
-  DEBUG = True
+    DEBUG = True
 else:
-  DEBUG = False
+    DEBUG = False
 OUTPUT_DIR_PATH = os.environ.get("OUTPUT_DIR_PATH")
 USER_AGENT = os.environ.get("USER_AGENT")
 
 if os.environ.get("ENV") == "PROD":
-  API_HOLDERS_URI = os.environ.get("API_HOLDERS_URI_PROD")
-  API_TRANSACTIONS_URI = os.environ.get("API_TRANSACTIONS_URI_PROD")
+    API_HOLDERS_URI = os.environ.get("API_HOLDERS_URI_PROD")
+    API_TRANSACTIONS_URI = os.environ.get("API_TRANSACTIONS_URI_PROD")
 else:
-  API_HOLDERS_URI = os.environ.get("API_HOLDERS_URI_DEV")
-  API_TRANSACTIONS_URI = os.environ.get("API_TRANSACTIONS_URI_DEV")
+    API_HOLDERS_URI = os.environ.get("API_HOLDERS_URI_DEV")
+    API_TRANSACTIONS_URI = os.environ.get("API_TRANSACTIONS_URI_DEV")
 
 API_HOLDERS_KEY = os.environ.get("API_HOLDERS_KEY")
 API_HOLDERS_RESOURCE = os.environ.get("API_HOLDERS_RESOURCE")
@@ -39,116 +39,116 @@ else:
 # Return list of holders
 # cbAppendtoCSV: callback function to save transactions to csv file
 def getTokenHolders(tokenName, tokenAddress, cbAppendtoCSV):
-  headers = { 'User-Agent': USER_AGENT }
-  query = {
-    "apiKey" : API_HOLDERS_KEY,
-    "limit" : API_HOLDERS_TOP_HOLDERS_LIMIT
-  }
-  URI = API_HOLDERS_URI + API_HOLDERS_RESOURCE + tokenAddress
-  response = requests.get(URI, params=query, headers = headers).json()
+    headers = { 'User-Agent': USER_AGENT }
+    query = {
+        "apiKey" : API_HOLDERS_KEY,
+        "limit" : API_HOLDERS_TOP_HOLDERS_LIMIT
+    }
+    URI = API_HOLDERS_URI + API_HOLDERS_RESOURCE + tokenAddress
+    response = requests.get(URI, params=query, headers = headers).json()
     
-  holders = response["holders"]  
-  for holder in holders:    
-    holderAddress = holder["address"]
-    
-    if DEBUG:
-      print (">>> HOLDER: " + holderAddress)
+    holders = response["holders"]  
+    for holder in holders:    
+        holderAddress = holder["address"]
         
-    getHolderTransactions(tokenName, holderAddress, cbAppendtoCSV)    
-    
-    if DEBUG:
-      print ("")
+        if DEBUG:
+            print (">>> HOLDER: " + holderAddress)
+            
+        getHolderTransactions(tokenName, holderAddress, cbAppendtoCSV)    
+        
+        if DEBUG:
+            print ("")
 
 
 # Return list of transactions
 # TODO: check if needs to do error/performance checking based on tranactions list size
 def getHolderTransactions(tokenName, holderAddress, cbAppendtoCSV):
-  page = 1
-  
-  headers = { 'User-Agent': USER_AGENT }
-  query = {
-    "module" : "account",
-    "action" : API_TRANSACTION_TYPE,
-    "address" : holderAddress,
-    "startblock" : 0,
-    "endblock" : 99999999,
-    "page" : page,
-    "offset" : API_TRANSACTION_PAGE_SIZE,
-    "sort" : "asc",
-    "apikey" : API_TRANSACTIONS_KEY
-  }
-  URI = API_TRANSACTIONS_URI + API_TRANSACTIONS_RESOURCE
+    page = 1
     
-  # FIXME: convert status to a bool, can't get it to work with bool(status)
-  status = "1"
-  while status == "1":
-    response = requests.get(URI, params=query, headers = headers).json()
-    status = response["status"]    
-    if status == "1":
-      transactions = response["result"]
-      addTransactionsEthPrice(transactions)
-      cbAppendtoCSV(tokenName, holderAddress, transactions)
-
-      if DEBUG:
-        print (">>> Transactions of holder: " + holderAddress)
-        print (">>> Page: " + str(page) + " Status: " + str(status))        
-        print (transactions[0])
-        break
+    headers = { 'User-Agent': USER_AGENT }
+    query = {
+        "module" : "account",
+        "action" : API_TRANSACTION_TYPE,
+        "address" : holderAddress,
+        "startblock" : 0,
+        "endblock" : 99999999,
+        "page" : page,
+        "offset" : API_TRANSACTION_PAGE_SIZE,
+        "sort" : "asc",
+        "apikey" : API_TRANSACTIONS_KEY
+    }
+    URI = API_TRANSACTIONS_URI + API_TRANSACTIONS_RESOURCE
       
-      page = page + 1
-      query["page"] = page
-        
-  return transactions
+    # FIXME: convert status to a bool, can't get it to work with bool(status)
+    status = "1"
+    while status == "1":
+        response = requests.get(URI, params=query, headers = headers).json()
+        status = response["status"]    
+        if status == "1":
+            transactions = response["result"]
+            addTransactionsEthPrice(transactions)
+            cbAppendtoCSV(tokenName, holderAddress, transactions)
+
+            if DEBUG:
+                print (">>> Transactions of holder: " + holderAddress)
+                print (">>> Page: " + str(page) + " Status: " + str(status))        
+                print (transactions[0])
+                break
+            
+            page = page + 1
+            query["page"] = page
+    
+    return transactions
 
 
 # Return transactions with ether price for each one
 def addTransactionsEthPrice(transactions):
-  for i, transaction in enumerate(transactions, start=0):
-    blockNumber = transaction["blockNumber"]
-    ethPrice = getEtherPrice(blockNumber)
-    transactions[i]["ethPrice"] = ethPrice
+    for i, transaction in enumerate(transactions, start=0):
+        blockNumber = transaction["blockNumber"]
+        ethPrice = getEtherPrice(blockNumber)
+        transactions[i]["ethPrice"] = ethPrice
 
-  return transactions
+    return transactions
 
 # Create csv file for tokenName and return it
 def createCSVFile(tokenName):
-  outFile = open(OUTPUT_DIR_PATH + tokenName + ".csv", 'w')
-  outFile.write("holderAddress,blockNumber,timeStamp,hash,nonce,blockHash,from,contractAddress,to,"\
-                "value,tokenName,tokenSymbol,tokenDecimal,transactionIndex,gas,gasPrice,gasUsed,"\
-                "cumulativeGasUsed,input,confirmations,ethPrice\n")
-  outFile.close()
+    outFile = open(OUTPUT_DIR_PATH + tokenName + ".csv", 'w')
+    outFile.write("holderAddress,blockNumber,timeStamp,hash,nonce,blockHash,from,contractAddress,to,"\
+                  "value,tokenName,tokenSymbol,tokenDecimal,transactionIndex,gas,gasPrice,gasUsed,"\
+                  "cumulativeGasUsed,input,confirmations,ethPrice\n")
+    outFile.close()
 
 
 # Return input in csv format
 def appendToCSV(tokenName, holderAddress, transactions):
-  outFile = open(OUTPUT_DIR_PATH + tokenName + ".csv", 'a')
-  csvWriter = csv.writer(outFile)
+    outFile = open(OUTPUT_DIR_PATH + tokenName + ".csv", 'a')
+    csvWriter = csv.writer(outFile)
     
-  for transaction in transactions:
-    transaction = [holderAddress] + list(transaction.values())
-    csvWriter.writerow(transaction)
-  
-  outFile.close()
+    for transaction in transactions:
+        transaction = [holderAddress] + list(transaction.values())
+        csvWriter.writerow(transaction)
+    
+    outFile.close()
 
 
 # Return tokens from json file
 def getTokens(filepath):
-  with open(filepath) as f:
-    return json.load(f)
+    with open(filepath) as f:
+        return json.load(f)
 
 
 # Main program
 def main():
-  tokensAddress = getTokens(TOKENS_FILEPATH)
-  for tokenName in tokensAddress:
-    if DEBUG:
-        print (">>> TOKEN:" + tokenName)
-      
-    createCSVFile(tokenName)
-    getTokenHolders(tokenName, tokensAddress[tokenName], appendToCSV)  
-    
-    if DEBUG:
-        print ("")
+    tokensAddress = getTokens(TOKENS_FILEPATH)
+    for tokenName in tokensAddress:
+        if DEBUG:
+            print (">>> TOKEN:" + tokenName)
+        
+        createCSVFile(tokenName)
+        getTokenHolders(tokenName, tokensAddress[tokenName], appendToCSV)  
+        
+        if DEBUG:
+            print ("")
 
 if __name__ == "__main__":
     main()
