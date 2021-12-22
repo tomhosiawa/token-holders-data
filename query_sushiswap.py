5381711453140170057fb86d80cd2e8bdbe6a0a8
Friendly Tin Man <80914620+jeainnybean@users.noreply.github.com>
2021-12-21 19:23:05 -0500

    Update query_sushiswap.py

--- query_sushiswap.py
+++ query_sushiswap.py
@@ -49,13 +49,47 @@ def getTopPairs():
 
     with open('output/wl_sushiswap_contracts.json', 'w', encoding='utf-8') as f:
         json.dump(pairs, f, ensure_ascii=False, indent=4)
-        
+
+#returns amount transacted in ETH given a txID
 def getEthAmountFromTx(txId):
-    #todo
+    query = f"""
+        query swaps {{
+      swaps(where:{{transaction:"{txId}"}}) {{
+        id
+        timestamp
+        amount0In
+        amount1In
+        amount0Out
+        amount1Out
+        amountUSD
+        pair {{
+          token0 {{
+            id
+            symbol
+          }}
+          token1 {{
+            id
+            symbol
+          }}
+        }}
+      }}
+    }}
+    """
+    result = runQuery(query)["swaps"]
 
+    #to handle liquidity removal/add or other non swap functions
+    if result == []:
+        return 0
+    else:
+        result = result[0]
 
-
-
+    print(result)
+    
+    #if token0 is eth or eth equivalent, return whatever amount of token 0 was transacted
+    if result['pair']['token0']['symbol'] == 'ETH' or result['pair']['token0']['symbol'] == 'WETH':
+        return max(float(result['amount0In']), float(result['amount0Out']))
+    else:
+        return max(float(result['amount1In']), float(result['amount1Out'])) 
 
 # Main program
 def main():
